c2eac3c7e23dbc7958b2e365fa6bff521a371ec2
zahrai-dev <zahrai.dev@gmail.com>
2023-01-06 00:42:32 +0700

    routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Routes, Link, useParams} from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Home(){
+    return <h2> Halaman Home </h2>
+}
+
+function ListView(){  
+    return(
+      <React.Fragment>
+        <h2> Semua User </h2>
+              <ul>
+                <Link to='/user/Naruto'> Naruto </Link> <br/>
+                <Link to='/user/Sasuke'> Sasuke </Link>
+              </ul>
+      </React.Fragment>
+
+      /* <div>
+        <h2> Semua User </h2>
+              <ul>
+                <Link to='/user/Naruto'> Naruto </Link> <br/>
+                <Link to='/user/Sasuke'> Sasuke </Link>
+              </ul>
+      </div> */
+    ) 
+}
+
+function DetailView(){
+  let { name } = useParams();
+  return <h2>Ini Halaman { name }</h2>
+}
+
+function NoMatch(){  
+  return  <h2> 404, Halaman tidak ditemukan. </h2>
+}
+
+class App extends Component {
+  render(){
+    return(
+    <BrowserRouter>
+        <div>
+          <nav>
+            <li> <Link to='/'> Home </Link></li>
+            <li> <Link to='/users'> User </Link></li>
+          </nav>
+          <main>
+            <Routes>
+              <Route path='/' exact element={<Home/>} />
+              <Route path='/users' exact element={<ListView/>} />
+              <Route path='/user/:name' exact element={<DetailView/>} />
+              <Route path='*' element={<NoMatch/>} />
+            </Routes>
+          </main>
+        </div>
+    </BrowserRouter>
+    );
+  }
 }
 
 export default App;
